39bd2a79ebcd2572f920f6c970fe51400ffbf61a
leooliveira <leooliveira@equinix.com>
2022-06-07 23:27:48 -0300

    Initial Commit

---
+++ src/modules/users/useCases/authenticateUser/AuthenticateUserUseCase.spec.ts
@@ -0,0 +1,30 @@
+import { hash } from 'bcryptjs';
+
+import { AuthenticateUserUseCase } from './AuthenticateUserUseCase';
+import { IUsersRepository } from '../../repositories/IUsersRepository';
+import { InMemoryUsersRepository } from '../../repositories/in-memory/InMemoryUsersRepository';
+
+let authenticateUseCase: AuthenticateUserUseCase;
+let usersRepository: IUsersRepository;
+
+describe('Authenticate User', () => {
+  beforeEach(() => {
+    usersRepository = new InMemoryUsersRepository();
+    authenticateUseCase = new AuthenticateUserUseCase(usersRepository);
+  });
+
+  it('should be able to authenticate an user', async () => {
+    const user = await usersRepository.create({
+      name: 'John Doe',
+      email: 'john@doe.com',
+      password: '123123'
+    });
+
+    const authUser = await authenticateUseCase.execute({
+      email: user.email,
+      password: '123123',
+    });
+
+    expect(authUser).toHaveProperty('token');
+  });
+});
